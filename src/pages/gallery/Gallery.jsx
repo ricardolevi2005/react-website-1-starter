0877efd2eccc0664d9cb4d8f44a79a0a1714ef96
Ricardo Levi <ricardolevi2005@hotmail.com>
2022-10-13 12:50:42 -0300

    feat: Create Gallery component

--- src/pages/gallery/Gallery.jsx
+++ src/pages/gallery/Gallery.jsx
@@ -1,7 +1,35 @@
-import './gallery.css'
+import { Header } from '../../components/Header';
+import HeaderImage from '../../images/header_bg_3.jpg';
+
+import './gallery.css';
 
 export const Gallery = () => {
-  return (
-    <div>Gallery</div>
-  )
-}
+
+  const galleryLenght = 15
+  const images = []
+
+  for(let i=1 ; i <= galleryLenght; i++)
+
+  images.push(require(`../../images/gallery${i}.jpg`))
+
+	return (
+		<>
+			<Header title='Our Gallery' image={HeaderImage}>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum recusandae
+				eligendi animi amet suscipit quasi voluptatum. Veniam at deserunt iste
+				voluptas.
+			</Header>
+      <section className="gallery">
+        <div className="container gallery__container">
+          {
+            images.map((image, index) => {
+              return <article key={index}>
+                <img src={image} alt={`Gallery Image ${index + 1}`} />
+              </article>
+            })
+          }
+        </div>
+      </section>
+		</>
+	);
+};
