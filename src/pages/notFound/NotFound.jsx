3bcfe0e904e6098acf1bdb86a7ad22041e44fce4
Ricardo Levi <ricardolevi2005@hotmail.com>
2022-10-13 13:05:57 -0300

    feat: Create a NotFound component

--- src/pages/notFound/NotFound.jsx
+++ src/pages/notFound/NotFound.jsx
@@ -1,7 +1,14 @@
-import './notFound.css'
+import { Link } from 'react-router-dom';
+
+import './notFound.css';
 
 export const NotFound = () => {
-  return (
-    <div>NotFound</div>
-  )
-}
+	return (
+		<section>
+			<div className='container notfound__container'>
+				<h2>Page Not Found</h2>
+        <Link to='/home' className='btn'>Go Back Home</Link>
+			</div>
+		</section>
+	);
+};
